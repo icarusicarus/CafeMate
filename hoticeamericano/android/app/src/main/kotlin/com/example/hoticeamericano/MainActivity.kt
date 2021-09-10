3631356bd670e7afff710491cf501ba52fdd83b4
redundant4u <rafch@naver.com>
2021-09-10 22:40:38 +0900

    Add login skeleton page

---
+++ hoticeamericano/android/app/src/main/kotlin/com/example/hoticeamericano/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hoticeamericano
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hoticeamericano/lib/Pages/LoginPage.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final List<TextEditingController> _controller = [ TextEditingController(), TextEditingController() ];
+  bool rememberCheck = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '안녕하세요.\n반갑습니다.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0
+                ),
+              )
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _controller[0],
+                    decoration: InputDecoration(
+                      hintText: '아이디',
+                    ),
+                    validator: (_value) {},
+                  ),
+                  TextFormField(
+                    controller: _controller[1],
+                    decoration: InputDecoration(
+                      hintText: '비밀번호',
+                    ),
+                    validator: (_value) {},
+                  ),
+                ],
+              )
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Checkbox(
+                    // title: Text('기억하기'),
+                    // controlAffinity: ListTileControlAffinity.leading,
+                    value: rememberCheck,
+                    onChanged: (_) {
+                      rememberCheck = !rememberCheck;
+                      setState(() {});
+                    },
+                  ),
+                  Text('기억하기')
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              child: Column(
+                children: <Widget>[
+                  ElevatedButton(
+                    child: Text('로그인'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.infinity, 30),
+                      padding: const EdgeInsets.all(15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      )
+                    ),
+                    onPressed: () {},
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 10.0)
+                  ),
+                  ElevatedButton(
+                    child: Text('회원가입'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.infinity, 30),
+                      padding: const EdgeInsets.all(15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      )
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              )
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
